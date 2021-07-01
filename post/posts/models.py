49b44ef0c25da3deab2f2352ccac75219ca4c09e
Jack Shuff <16825267+JWShuff@users.noreply.github.com>
2021-07-01 15:34:02 -0400

    Added str methods, and the first few posts. Going to build a seed data file to do the initial population.

--- post/posts/models.py
+++ post/posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.CharField(max_length=1024)
+
+    def __str__(self) -> str:
+        return f"ID: {self.id} \nTitle: {self.title}"
+
+class Comment(models.Model):
+    body = models.CharField(max_length=1024)
+    post = models.ForeignKey(Post,
+    on_delete=models.CASCADE,
+    related_name='comments')
+
+    def __str__(self) -> str:
+        return f"ID: {self.id} \nOn Post: {self.post}\nText: {self.body[:60]}..."
--- post/posts/views.py
+++ post/posts/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+def get_post_id(post_id):
+    return Post.objects.get(id=post_id)
+
 def index(request):
     return HttpResponse("Hello, world. You're at the posts index.")
 
