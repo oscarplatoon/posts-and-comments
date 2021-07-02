d267b01ca119cb3dd2c2975aa704ea9b73f15cb0
Jack Shuff <16825267+JWShuff@users.noreply.github.com>
2021-07-01 20:30:22 -0400

    Index mostly works, clicking into a post is still broken. Using the wrong logic in index.html I think.

--- post/posts/views.py
+++ post/posts/views.py
@@ -1,11 +1,32 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-def get_post_id(post_id):
+from .models import Post, Comment
+
+def get_post(post_id):
     return Post.objects.get(id=post_id)
 
+def get_all_posts():
+    return Post.objects.all()
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the posts index.")
+    post_list = get_all_posts()
+    post_data = {'posts':post_list}
+    return render(request, 'posts/index.html', post_data)
 
 def detail(request, post_id):
-    return HttpResponse(f"You're looking at post {post_id}.")
+    post = get_post(post_id)
+    try:
+        post_comments = post.comments.all()
+    except(KeyError, Comment.DoesNotExist):
+        return render(request,
+        'posts/detail.html',
+        {
+            'post':post,
+            'error_message' : "This post has no comments."
+        })
+    else:
+        return render(request,
+        'posts/detail.html',
+        post_comments)
