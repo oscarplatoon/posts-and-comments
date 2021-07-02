814fcb2f1ac11f439369431d018d17ef3dc28dba
Ankur Shah <ankur@codeplatoon.org>
2021-07-02 11:45:05 -0400

    completed release 1

---
+++ posts_app/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from posts_app import views
+
+# blog/posts/5
+
+urlpatterns = [
+    path("posts/", views.index, name="post-list"),
+    path("posts/<int:post_id>", views.post_detail, name="post-detail")
+]
--- posts_app/views.py
+++ posts_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hey, INDEX route is working!!!")
+    
+
+def post_detail(request, post_id):
+    return HttpResponse(f"Hey, POST_DETAIL for item {post_id} route is working!!!")
