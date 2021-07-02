adcfb285c7b6d7169be8328efa06cad279680f82
Ankur Shah <ankur@codeplatoon.org>
2021-07-02 11:58:31 -0400

    completed release 2

--- posts_app/models.py
+++ posts_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=64)
+    body = models.TextField()
+    name = models.CharField(max_length=32)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    body = models.CharField(max_length=128)
+    name = models.CharField(max_length=32)
